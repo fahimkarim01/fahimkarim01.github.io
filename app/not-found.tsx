b040908a1cee748171f9d55b0a9605611acdc595
Md. Fahim Karim <23-50446-1@student.aiub.edu>
2026-06-29 14:02:20 +0600

    Fix 404 prerender build failure on Vercel

---
+++ app/not-found.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+
+export default function NotFound() {
+  return (
+    <main style={{ padding: '4rem 1.5rem', textAlign: 'center' }}>
+      <h1 style={{ fontSize: '2rem', fontWeight: 700 }}>Page not found</h1>
+      <p style={{ marginTop: '1rem' }}>The page you are looking for does not exist.</p>
+      <p style={{ marginTop: '1.5rem' }}>
+        <Link href="/">Go back home</Link>
+      </p>
+    </main>
+  )
+}
