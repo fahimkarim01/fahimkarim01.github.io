11c1738abdd376672fc23b259ea1fb6547c627d4
Md. Fahim Karim <23-50446-1@student.aiub.edu>
2026-06-29 12:38:14 +0600

    Initial: academic portfolio

---
+++ app/[[...mdxPath]]/page.tsx
@@ -0,0 +1,34 @@
+import { Metadata } from 'next'
+import { generateStaticParamsFor, importPage } from 'nextra/pages'
+import { useMDXComponents as getMDXComponents } from '../../mdx-components'
+
+export const generateStaticParams = generateStaticParamsFor('mdxPath')
+
+type PageProps = {
+  params: Promise<{
+    mdxPath?: string[]
+  }>
+}
+
+export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
+  const { mdxPath } = await params
+  const { metadata } = await importPage(mdxPath)
+  return metadata as Metadata
+}
+
+const Wrapper = getMDXComponents().wrapper
+
+export default async function Page(props: PageProps) {
+  const params = await props.params
+  const { default: MDXContent, ...wrapperProps } = await importPage(params.mdxPath)
+
+  if (!Wrapper) {
+    return <MDXContent {...props} params={params} />
+  }
+
+  return (
+    <Wrapper {...wrapperProps}>
+      <MDXContent {...props} params={params} />
+    </Wrapper>
+  )
+}
