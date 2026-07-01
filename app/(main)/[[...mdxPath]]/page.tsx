ad0e34b2242c8d29fcd234e5e8520be142f71e22
Md. Fahim Karim <23-50446-1@student.aiub.edu>
2026-07-01 16:11:20 +0600

    Remove wrapper from page.tsx to fix ConfigContext crash during static prerender

--- app/(main)/[[...mdxPath]]/page.tsx
+++ app/(main)/[[...mdxPath]]/page.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next'
 import { importPage, generateStaticParamsFor } from 'nextra/pages'
-import { useMDXComponents as getMDXComponents } from '../../../mdx-components'
 
 export const generateStaticParams = generateStaticParamsFor('mdxPath')
 
@@ -12,37 +11,13 @@ type PageProps = {
 
 export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
   const { mdxPath } = await params
-  try {
-    const { metadata } = await importPage(mdxPath)
-    return metadata
-  } catch (error) {
-    console.error('[nextra] generateMetadata importPage failed', { mdxPath, error })
-    throw error
-  }
+  const { metadata } = await importPage(mdxPath)
+  return metadata ?? {}
 }
 
 export default async function Page(props: PageProps) {
   const params = await props.params
   const { mdxPath } = params
-  let page
-
-  try {
-    page = await importPage(mdxPath)
-  } catch (error) {
-    console.error('[nextra] Page importPage failed', { mdxPath, error })
-    throw error
-  }
-
-  const { default: MDXContent, ...wrapperProps } = page
-  const Wrapper = getMDXComponents().wrapper
-
-  if (!Wrapper) {
-    return <MDXContent {...props} params={params} />
-  }
-
-  return (
-    <Wrapper {...wrapperProps}>
-      <MDXContent {...props} params={params} />
-    </Wrapper>
-  )
+  const { default: MDXContent } = await importPage(mdxPath)
+  return <MDXContent {...props} params={params} />
 }
